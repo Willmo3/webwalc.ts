2ea4bf71f9ba8739138485395973576477c94f1b
Will Morris <morris.william.work@gmail.com>
2024-09-01 17:13:00 -0400

    Add small repl

--- index.ts
+++ index.ts
@@ -1,4 +1,22 @@
 import {lex} from "./lexer";
+import * as fs from "node:fs";
 
-const program = "3 + 5 - -8";
-console.log(lex(program));
+import * as readline from "node:readline";
+
+// Must be two or three args
+if (process.argv.length < 2 || process.argv.length > 3) {
+    console.log("Invalid args!");
+    console.log("Usage: npm run start [path to file]");
+    process.exit(1);
+}
+
+const input = process.argv.length == 2 ? process.stdin : fs.createReadStream(process.argv[3]);
+
+const rl = readline.createInterface({
+    input: input,
+    output: process.stdout,
+});
+
+for await (const line of rl) {
+    console.log(lex(line));
+}
